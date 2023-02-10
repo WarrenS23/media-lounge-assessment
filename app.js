e78f4106a79df9d1649c768eeb7939d356642683
Warren Strydom <ws@vectra.co.za>
2023-02-10 22:03:01 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Navbar 
+var x = document.getElementById("myTopnav");
+function myFunction() {
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+// Swiper 
+const settings = {
+    loop: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets"
+    },
+    // autoplay: {
+    //     delay: 5000,
+    //   },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  };
+  
+  const swiper = new Swiper(".mySwiper", settings);
+  
